834eb9038a9c45fd63211cf75b49e29431542e07
Orisunmibare Muiz Olamilekan <orismuiz2003@gmail.com>
2025-11-25 11:23:19 +0100

    feat: Added a dropdown hamburger to mobile view nav

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+const displayMenu = ()=>{
+    let dropDown = document.getElementById("dropDown");
+    if(dropDown.style.height === "0px"){
+        dropDown.style.height = "250px"
+    }
+    else{
+        dropDown.style.height ="0px"
+    }
+
+}
